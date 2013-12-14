ab173dd0d69cf3bd2f24b140aeb8e0efe9ad8442
tannerjfco <tannerjfco@gmail.com>
2013-12-14 09:47:29 -0700

    Update mysql.rb

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -28,7 +28,7 @@ Chef::Log::debug "drupal::mysql passwords = #{passwords.inspect}"
 if Chef::Config[:solo]
   Chef::Log.debug "drupal::mysql Setting chef solo node mysql passwords."
   node.set['mysql']['server_debian_password'] = passwords["debian"] unless passwords["debian"].nil?
-  node[:mysql][:server_root_password] = passwords["root"] unless passwords["root"].nil?
+  node.default[:mysql][:server_root_password] = passwords["root"] unless passwords["root"].nil?
   node.set['mysql']['server_repl_password'] = passwords["replication"] unless passwords["replication"].nil?
 end
 
