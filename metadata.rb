6b902177ec56c977066be8d36418e0ca83ecfeff
Alex Knoll <alex@newmediadenver.com>
2014-04-21 14:25:18 -0600

    Add reference to drupal::apache ref

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ license          'Apache 2.0'
 description      'Manages Drupal'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.0'
+recipe            'drupal::apache', 'Loads and configures apache'
 
 depends 'apache2'
 depends 'mysql'
