e3f0df0457e00fdc368fff23b534434e37fb9818
Alex Knoll <alex@newmediadenver.com>
2014-04-30 08:02:32 -0600

    include mod ssl and mod fastcgi

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -20,6 +20,8 @@
 include_recipe 'apache2'
 include_recipe 'apache2::mod_php5'
 include_recipe 'apache2::mod_rewrite'
+include_recipe "apache2::mod_fastcgi"
+include_recipe "apache2::mod_ssl"
 
 node[:drupal][:sites].each do |site_name, site|
   if site[:active]
