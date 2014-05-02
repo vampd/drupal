cecf86d9beebebb02222181fb742befceee9c947
Alex Knoll <alex@newmediadenver.com>
2014-05-02 10:54:18 -0600

    Add remove ability to apache recipe

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -17,10 +17,45 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+service 'apache2' do
+  case node[:platform_family]
+  when 'rhel', 'fedora', 'suse'
+    service_name 'httpd'
+    # If restarted/reloaded too quickly httpd has a habit of failing.
+    # This may happen with multiple recipes notifying apache to restart - like
+    # during the initial bootstrap.
+    restart_command '/sbin/service httpd restart && sleep 1'
+    reload_command '/sbin/service httpd reload && sleep 1'
+  when 'debian'
+    service_name 'apache2'
+    restart_command '/usr/sbin/invoke-rc.d apache2 restart && sleep 1'
+    reload_command '/usr/sbin/invoke-rc.d apache2 reload && sleep 1'
+  when 'arch'
+    service_name 'httpd'
+  when 'freebsd'
+    service_name 'apache22'
+  end
+  supports [:restart, :reload, :status]
+  action :enable
+end
 
 node[:drupal][:sites].each do |site_name, site|
   if site[:active]
     site['web_app'].each do |port, app|
+
+      # Remove virtualhost settings if action = remove
+      file "#{node[:drupal][:server][:available]}/#{site_name}-#{port}.conf" do
+        action :delete
+        only_if { site[:deploy][:action].any? { |action| action == 'remove' } }
+        notifies :reload, 'service[apache2]'
+      end
+
+      file "#{node[:drupal][:server][:enabled]}/#{site_name}-#{port}.conf" do
+        action :delete
+        only_if { site[:deploy][:action].any? { |action| action == 'remove' } }
+        notifies :reload, 'service[apache2]'
+      end
+
       drupal_web_app "#{site_name}-#{port}" do
         port port
         docroot app['docroot'] unless app['docroot'].nil?
