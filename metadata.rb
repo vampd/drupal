921a58e36856e64e3b449035fd29873f86d6fa8c
Alex Knoll <alex@newmediadenver.com>
2014-06-24 13:51:32 -0600

    Update drupal and nmd-drupal to nmddrupal

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 # encoding: utf-8
-name 'nmd-drupal'
+name 'nmddrupal'
 maintainer 'New Media Denver'
 maintainer_email 'support@newmediadenver.com'
 license 'Apache 2.0'
-description 'Installs/Configures nmd-drupal'
+description 'Installs/Configures nmddrupal'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '3.0.0'
 supports 'ubuntu', '>= 12.04'
@@ -15,110 +15,110 @@ description desc
 depends 'git'
 
 long_description 'Manages the installation and configuration of Drupal.'
-recipe 'nmd-drupal::default', desc
-recipe 'nmd-drupal::files', 'Manages files'
+recipe 'nmddrupal::default', desc
+recipe 'nmddrupal::files', 'Manages files'
 
-grouping 'nmd-drupal/drush',
+grouping 'nmddrupal/drush',
          title: 'Drush attributes',
          description: 'Drush recipe attributes'
 
-attribute 'nmd-drupal/drush/revision',
-          display_name: '[:drupal][:drush][:revision]',
+attribute 'nmddrupal/drush/revision',
+          display_name: '[:nmddrupal][:drush][:revision]',
           description: 'This is the version of drush to install.',
           type: 'string',
           required: 'recommended',
-          recipes: ['nmd-drupal::drush'],
+          recipes: ['nmddrupal::drush'],
           default: '6.3.0'
 
-attribute 'nmd-drupal/drush/repository',
-          display_name: '[:drupal][:drush][:repository]',
+attribute 'nmddrupal/drush/repository',
+          display_name: '[:nmddrupal][:drush][:repository]',
           description: 'This is the code repository to reference.',
           type: 'string',
           required: 'recommended',
-          recipes: ['nmd-drupal::drush'],
+          recipes: ['nmddrupal::drush'],
           default: 'https://github.com/drush-ops/drush.git'
 
-attribute 'nmd-drupal/drush/dir',
-          display_name: '[:drupal][:drush][:dir]',
+attribute 'nmddrupal/drush/dir',
+          display_name: '[:nmddrupal][:drush][:dir]',
           description: 'This folder stores the clone repository.',
           type: 'string',
           required: 'recommended',
-          recipes: ['nmd-drupal::drush'],
+          recipes: ['nmddrupal::drush'],
           default: '/opt/drush'
 
-attribute 'nmd-drupal/drush/executable',
-          display_name: '[:drupal][:drush][:executable]',
+attribute 'nmddrupal/drush/executable',
+          display_name: '[:nmddrupal][:drush][:executable]',
           description: 'This is the symlinked file to the drush binary.',
           type: 'string',
           required: 'recommended',
-          recipes: ['nmd-drupal::drush'],
+          recipes: ['nmddrupal::drush'],
           default: '/usr/bin/drush'
 
-attribute 'nmd-drupal/drush/owner',
-          display_name: '[:drupal][:drush][:owner]',
+attribute 'nmddrupal/drush/owner',
+          display_name: '[:nmddrupal][:drush][:owner]',
           description: 'This is owner of the executable binary.',
           type: 'string',
           required: 'recommended',
-          recipes: ['nmd-drupal::drush'],
+          recipes: ['nmddrupal::drush'],
           default: 'root'
 
-attribute 'nmd-drupal/drush/group',
-          display_name: '[:drupal][:drush][:group]',
+attribute 'nmddrupal/drush/group',
+          display_name: '[:nmddrupal][:drush][:group]',
           description: 'This is group of the executable binary.',
           type: 'string',
           required: 'recommended',
-          recipes: ['nmd-drupal::drush'],
+          recipes: ['nmddrupal::drush'],
           default: 'root'
 
-attribute 'nmd-drupal/drush/mode',
-          display_name: '[:drupal][:drush][:mode]',
+attribute 'nmddrupal/drush/mode',
+          display_name: '[:nmddrupal][:drush][:mode]',
           description: 'This is permissions of the executable binary.',
           type: 'string',
           required: 'recommended',
-          recipes: ['nmd-drupal::drush'],
+          recipes: ['nmddrupal::drush'],
           default: '755'
 
-attribute 'nmd-drupal/drush/state',
-          display_name: '[:drupal][:drush][:state]',
+attribute 'nmddrupal/drush/state',
+          display_name: '[:nmddrupal][:drush][:state]',
           description: 'Controls runtime action: install, update, or purge',
           type: 'string',
           required: 'recommended',
-          recipes: ['nmd-drupal::drush'],
+          recipes: ['nmddrupal::drush'],
           default: 'install'
 
-grouping 'nmd-drupal/files',
+grouping 'nmddrupal/files',
          title: 'Files attributes',
          description: 'Files recipe attributes'
 
-attribute 'drupal/files/path',
-          display_name: '[:drupal][:files][:path]',
+attribute 'nmddrupal/files/path',
+          display_name: '[:nmddrupal][:files][:path]',
           description: 'This is the root level files directory path.',
           type: 'string',
           required: 'recommended',
-          recipes: ['nmd-drupal::files'],
+          recipes: ['nmddrupal::files'],
           default: '/default/files'
 
-attribute 'drupal/files/owner',
-          display_name: '[:drupal][:files][:owner]',
+attribute 'nmddrupal/files/owner',
+          display_name: '[:nmddrupal][:files][:owner]',
           description: 'This is the root level files directory owner.',
           type: 'string',
           required: 'recommended',
-          recipes: ['nmd-drupal::files'],
+          recipes: ['nmddrupal::files'],
           default: 'root'
 
-attribute 'drupal/files/group',
-          display_name: '[:drupal][:files][:path]',
+attribute 'nmddrupal/files/group',
+          display_name: '[:nmddrupal][:files][:path]',
           description: 'This is the root level files directory path group owner
           .',
           type: 'string',
           required: 'recommended',
-          recipes: ['nmd-drupal::files'],
+          recipes: ['nmddrupal::files'],
           default: 'root'
 
-attribute 'drupal/files/mode',
-          display_name: '[:drupal][:files][:mode]',
+attribute 'nmddrupal/files/mode',
+          display_name: '[:nmddrupal][:files][:mode]',
           description: 'This is the root level files directory mode.',
           type: 'string',
           required: 'recommended',
-          recipes: ['nmd-drupal::files'],
+          recipes: ['nmddrupal::files'],
           default: '0755'
