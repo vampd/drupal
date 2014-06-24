a74539bc4ee11ce6a1e6f29f619e7e0ffb17aa9a
Alex Knoll <alex@newmediadenver.com>
2014-06-24 16:15:45 -0600

    update all attributes to be nmd namespaced

--- attributes/drush.rb
+++ attributes/drush.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 #
-# Cookbook Name:: nmd-drupal
+# Cookbook Name:: nmddrupal
 # Attributes:: files
 #
 # Author:: NEWMEDIA Denver
@@ -18,18 +18,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default[:drupal][:drush][:revision] = '6.3.0'
-default[:drupal][:drush][:repository] =
+default[:nmddrupal][:drush][:revision] = '6.3.0'
+default[:nmddrupal][:drush][:repository] =
   'https://github.com/drush-ops/drush.git'
-default[:drupal][:drush][:dir] = '/opt/drush'
-default[:drupal][:drush][:executable] = '/usr/bin/drush'
-default[:drupal][:drush][:owner] = 'root'
-default[:drupal][:drush][:group] = 'root'
-default[:drupal][:drush][:mode] = '0755'
+default[:nmddrupal][:drush][:dir] = '/opt/drush'
+default[:nmddrupal][:drush][:executable] = '/usr/bin/drush'
+default[:nmddrupal][:drush][:owner] = 'root'
+default[:nmddrupal][:drush][:group] = 'root'
+default[:nmddrupal][:drush][:mode] = '0755'
 
 # States control what happens during the run. Listed below are the start and
 # end states that we trigger events from.
 # - install -> installed
 # - update -> updated
 # - purge -> purged
-default[:drupal][:drush][:state] = 'install'
+default[:nmddrupal][:drush][:state] = 'install'
