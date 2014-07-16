e2c8def46b43d56c43c65741693d88ccb33979e3
Kevin Bridges <kevin@cyberswat.com>
2014-07-16 09:47:49 -0600

    Moving over to a legitimate LWRP.

---
+++ providers/code.rb
@@ -0,0 +1,60 @@
+#
+# Cookbook Name:: drupal
+# Provider:: code
+#
+# Copyright 2010-2013, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+use_inline_resources if defined?(use_inline_resources)
+
+def whyrun_supported?
+  true
+end
+
+action :create do
+
+  directory new_resource.path do
+    owner new_resource.owner
+    group new_resource.group
+    mode new_resource.mode
+    action :create
+    recursive true
+    not_if { ::File.exist?(new_resource.path) }
+  end
+
+  ssh_known_hosts_entry URI.parse(new_resource.repository).host
+
+  deploy new_resource.path do
+    repository new_resource.repository
+    revision new_resource.revision
+    keep_releases new_resource.releases
+    symlink_before_migrate.clear
+    create_dirs_before_symlink.clear
+    purge_before_symlink.clear
+    symlinks.clear
+  end
+
+  new_resource.updated_by_last_action(true)
+end
+
+action :delete do
+
+  directory new_resource.path do
+    action :delete
+    recursive true
+    only_if { ::File.exist?(new_resource.path) }
+  end
+
+  new_resource.updated_by_last_action(true)
+end
---
+++ resources/code.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: nmddrupal
+# Resource:: code
+#
+# Copyright 2008-2014, Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+actions :create, :delete
+default_action :create
+
+attribute :path, kind_of: String, name_attribute: true
+attribute :owner, kind_of: String
+attribute :group, kind_of: String
+attribute :mode, kind_of: Fixnum, default: 00755
+attribute :repository, kind_of: String, default: 'http://git.drupal.org/project/drupal.git'
+attribute :revision, kind_of: String, default: '7.x'
+attribute :releases, kind_of: Fixnum, default: 5
+
+def initialize(*args)
+  super
+  @action = :create
+end
