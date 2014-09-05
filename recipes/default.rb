398a04d1843c40b22206743630440a66b9d42284
Tim Whitney <tim.d.whitney@gmail.com>
2014-09-05 15:32:29 -0600

    Added change that allows for the admin user db and password to be set by chef on import of an existing database

--- recipes/default.rb
+++ recipes/default.rb
@@ -331,6 +331,20 @@ node[:drupal][:sites].each do |site_name, site|
       EOH
     end
 
+    bash "drush-update-admin-password-on-import" do
+      cwd "#{base}/current/#{site[:drupal][:settings][:docroot]}"
+      user 'root'
+      cmd = "drush upwd #{drupal_user['admin_user']} --password=#{drupal_user['admin_pass']}"
+      only_if { site[:deploy][:action].any? { |action| action == 'import' } }
+
+      Chef::Log.debug("Drupal::default: before_restart: execute: #{cmd.inspect}") if site[:deploy][:action].any? { |action| action == 'import' }
+      code <<-EOH
+        set -x
+        set -e
+        #{cmd}
+      EOH
+    end
+
     bash "drush-site-update-#{site_name}" do
       cwd "#{base}/current/#{site[:drupal][:settings][:docroot]}"
       user 'root'
