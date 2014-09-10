c6de287c91c4bd0e871a808824bca4832885cf75
Ryan McVeigh <rymcveigh@gmail.com>
2014-09-10 14:50:55 -0600

    changed node recipe

--- recipes/nodejs.rb
+++ recipes/nodejs.rb
@@ -1,10 +1,10 @@
-if node[:nodejs]
+if node[:drupal][:nodejs]
   include_recipe "nodejs"
   include_recipe "nodejs::install_from_package"
   include_recipe "nodejs::npm"
-  if node[:nodejs][:packages]
+  if node[:drupal][:nodejs][:packages]
     cmd = ""
-    node[:nodejs][:packages].each do |package|
+    node[:drupal][:nodejs][:packages].each do |package|
       cmd << "npm install -g #{package};"
     end
     bash "Install NPM Packages" do
