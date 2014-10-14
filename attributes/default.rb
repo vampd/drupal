9fc96b9ad0d39512b38eee018eb9523e48fcd0c7
Tim Whitney <tim.d.whitney@gmail.com>
2014-10-14 10:21:08 -0600

    Added some redunkulous check

--- attributes/default.rb
+++ attributes/default.rb
@@ -52,9 +52,13 @@ node[:drupal][:sites].each do |site_name, site|
   default[:drupal][:sites][site_name][:drupal][:install]['--clean-url'] = 1
 
   #Set up the docroot to be used as a default
-  if site[:drupal][:settings][:docroot].empty?
-    default[:drupal][:sites][site_name][:drupal][:settings][:docroot] = ''
-    docroot = ''
+  if site[:drupal].empty?
+    if site[:drupal][:settings].empty?
+      if site[:drupal][:settings][:docroot].empty?
+        default[:drupal][:sites][site_name][:drupal][:settings][:docroot] = ''
+        docroot = ''
+      end
+    end
   else
     docroot = site[:drupal][:settings][:docroot]
   end
